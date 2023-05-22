e97a2284e4f821ed84103fa534941c280140b8f3
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-05-22 14:21:32 +0530

    updated file - layouts/default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="min-h-screen bg-slate-100">
+            <Nuxt />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({}),
+};
+</script>
+
+<style scoped></style>
